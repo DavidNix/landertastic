2b9e85f65f4ef2db11dac694f1922cb7cb5c2bc5
David Nix <davidnix@gospotcheck.com>
2013-10-06 21:33:11 -0600

    Add sessions controller.

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Admin::SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Landertastic::Application.routes.draw do
   root "home#index"
+
+  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
