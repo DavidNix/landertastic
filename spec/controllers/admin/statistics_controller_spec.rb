a30d0263f091af07fd12530d69b4bf207db207d4
David Nix <davidnix@gospotcheck.com>
2013-10-13 22:42:34 -0600

    Add reset statistics button and expand functionality.

--- spec/controllers/admin/statistics_controller_spec.rb
+++ spec/controllers/admin/statistics_controller_spec.rb
@@ -17,7 +17,31 @@ describe Admin::StatisticsController do
 
         expect(response).to be_success
         expect(response).to render_template("layouts/admin")
+        expect(assigns(:statistics)).to_not be_nil
       end
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      5.times { create(:hit) }
+      3.times { create(:lead) }
+    end
+    it "destroys all data and redirects" do
+      sign_in_admin
+      delete :destroy
+
+      expect(Lead.count).to eq 0
+      expect(Hit.count).to eq 0
+      expect(flash[:notice]).to_not be_nil
+      expect(response).to redirect_to(admin_statistics_path)
+    end
+
+    it "does not allow unauthenticated admin to destroy data" do
+      delete :destroy
+
+      expect(Hit.count).to eq 5
+      expect(Lead.count).to eq 3
     end
   end
 end
